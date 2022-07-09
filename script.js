43406d52ba8bc649a295f787a520090331e4b2ea
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2022-07-09 22:15:28 +0300

    Fixed carousel problem

--- script.js
+++ script.js
@@ -10,20 +10,39 @@ async function getProject() {
 }
 
 project = getProject()
-let project_card = `<div class="card col-4" style="width: 18rem;box-shadow:none;">
-<img src="StyleSheet/Working.jpg " class="card-img-top img-fluid mx-auto ">
-<div class="card-body ">
-    <h5 class="card-title project-name"></h5>
-    <p class="card-text ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <center>
-        <a class="github-ref">
-            <i class="fa-brands fa-github fa-2x "></i>
-        </a>
-    </center>
-</div>`
-let length = document.getElementsByClassName("carousel-inner")[1].getElementsByClassName('carousel-item').length
-let carousel_item = document.getElementsByClassName("carousel-inner")[1].childNodes
-for (let index = 0; index < carousel_item.length; index++) {
-    console.log(carousel_item[index])
+let project_card = `<div class="carousel-item container">
+<div class="row justify-content-center ">
+    <img class="col-lg-8 " src="StyleSheet/Working.jpg " />
+    <div class="col-xl-3 col-md-4 card ">
+        <div class="card-header text-center ">
+            Featured
+        </div>
+        <img src="StyleSheet/Working.jpg " class="card-img " />
+        <div class="card-body ">
+            <h5 class="card-title ">Special title treatment
 
-}
+            </h5>
+            <p class="card-text ">With supporting text below as a natural lead-in to additional content.</p>
+            <center>
+                <a class="github-ref ">
+                    <i class="fa-brands fa-github fa-2x "></i>
+                </a>
+            </center>
+        </div>
+        <div class="card-footer text-muted text-center ">
+            2 days ago
+        </div>
+    </div>
+</div>
+
+</div>`
+
+
+
+
+/*let length = document.getElementsByClassName("carousel-inner")[1].getElementsByClassName('carousel-item').length
+    let carousel_item = document.getElementsByClassName("carousel-inner")[1].childNodes
+    for (let index = 0; index < carousel_item.length; index++) {
+        console.log(carousel_item[index])
+
+    }*/
