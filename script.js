fef422b461de859e5b6c38bc317f47c59038656f
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2022-08-29 17:27:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,81 @@
 async function getProject() {
-   try{
-    let response = await fetch("https://api.github.com/users/GabrieLempert/repos",{
-        auth: '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'
-        });
-   }catch(err){
-      alert("Sorry the GitAPI reached is limit")
-   }
-    try{
+  try {
+    let response = await fetch(
+        "https://api.github.com/users/GabrieLempert/repos",
+        {auth: "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"});
         let projects = await response.json();
-    }catch(err){
-        alert("Somthing Went wrong");
-    }
-    return projects;
-
+        return projects;
+  } catch (error) {
+    alert("Sorry I reached GitHubAPI rate limit")
+    let response = await fetch("projects.json");
+        let projects = await response.json();
+        return projects;
+  }
 }
 
-
-function changeString(str){
-    let arr=str.split("-");
-    let newStr=arr[0];
-    for (let index = 1; index < arr.length; index++) {
-       newStr=newStr+" "+arr[index];
-    }
-    return newStr;
-    
+function changeString(str) {
+  let arr = str.split("-");
+  let newStr = arr[0];
+  for (let index = 1; index < arr.length; index++) {
+    newStr = newStr + " " + arr[index];
+  }
+  return newStr;
 }
 
 async function createNewElement() {
-    let projectList = document.getElementById("project-list");
-    let projects = await getProject();
-    for (let index = 0; index < projects.length; index++) {
-        let entry = document.createElement('li');
-        entry.className ="project-item";
-        entry.appendChild(document.createTextNode(`${changeString(projects[index].name)}`))
-        projectList.appendChild(entry);
-    }
+  let projectList = document.getElementById("project-list");
+  let projects = await getProject();
+  for (let index = 0; index < projects.length; index++) {
+    let entry = document.createElement("li");
+    entry.className = "project-item";
+    entry.appendChild(
+      document.createTextNode(`${changeString(projects[index].name)}`)
+    );
+    projectList.appendChild(entry);
+  }
 }
 
-async function showProjectInfo(){
-    await createNewElement()
-    let projectInList = document.getElementById("project-list").getElementsByClassName("project-item");
-    let header=document.getElementsByClassName("project-info")[0].getElementsByTagName("h4")[0];
-    let descp=document.getElementsByClassName("project-info")[0].getElementsByTagName("p")[0];
-    let projectLink=document.getElementById("project-link");
-    console.log(header);
-    let project = await getProject();  
-    for (let index = 0; index < projectInList.length; index++) {
-        projectInList[index].addEventListener("click",function(){
-            header.innerHTML=`${changeString(project[index].name)}`
-            if(project[index].description!==null)
-            descp.innerHTML =`${changeString(project[index].description)}`
-            else
-            descp.innerHTML="Add description";
-            projectLink.href=project[index].html_url;
-
-        });    
-    }
+async function showProjectInfo() {
+  await createNewElement();
+  let projectInList = document
+    .getElementById("project-list")
+    .getElementsByClassName("project-item");
+  let header = document
+    .getElementsByClassName("project-info")[0]
+    .getElementsByTagName("h4")[0];
+  let descp = document
+    .getElementsByClassName("project-info")[0]
+    .getElementsByTagName("p")[0];
+  let projectLink = document.getElementById("project-link");
+  console.log(header);
+  let project = await getProject();
+  for (let index = 0; index < projectInList.length; index++) {
+    projectInList[index].addEventListener("click", function () {
+      header.innerHTML = `${changeString(project[index].name)}`;
+      if (project[index].description !== null)
+        descp.innerHTML = `${changeString(project[index].description)}`;
+      else descp.innerHTML = "Add description";
+      projectLink.href = project[index].html_url;
+    });
+  }
 }
-function showJobInfo(){
-    let jobsObj ={
-        "QA and NOC":"","Security":"","Commander":""};
-    const arrowToJob=document.getElementById("jobs").getElementsByTagName("i");
-    const jobs= document.getElementsByClassName("work-info");
-    for(let i=0;i<arrowToJob.length-1;i++){
-        arrowToJob[i].addEventListener("click",function(){
-            jobs[0].getElementsByTagName("h5")[0].innerHTML=Object.keys(jobsObj)[i];
-            jobs[0].getElementsByTagName("p")[0].innerHTML=jobsObj[i];
-            jobs[0].style.visibility = 'visible'; 
-        });
-    }
+function showJobInfo() {
+  let jobsObj = {
+    "QA and NOC":
+      "Responsible for the servers continuous function, communication with programmers and clients, releasing products to clients using linux, checking products in production.",
+    Security: "Responsible for the safety of workers and the facilities.",
+    Commander:
+      "Responsible for the safety, training, and general well-being of soldiers, as well as responsible for the maintenance of the tools.",
+  };
+  const arrowToJob = document.getElementById("jobs").getElementsByTagName("i");
+  const jobs = document.getElementsByClassName("work-info");
+  for (let i = 0; i < arrowToJob.length - 1; i++) {
+    arrowToJob[i].addEventListener("click", function () {
+      jobs[0].getElementsByTagName("h5")[0].innerHTML = Object.keys(jobsObj)[i];
+      jobs[0].getElementsByTagName("p")[0].innerHTML = jobsObj[i];
+      jobs[0].style.visibility = "visible";
+    });
+  }
 }
 showJobInfo();
 showProjectInfo();
